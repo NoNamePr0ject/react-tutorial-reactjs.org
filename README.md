f5eb18f76491aeb18b8c7473b335ef9bb73768cb
NoNamePr0ject <slu.peja92@gmail.com>
2021-10-19 12:46:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-48kk4c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-48kk4c)
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const User = {
+  name: 'Rafał',
+  lastname: 'Nowak',
+};
+
+function showUser(user) {
+  if (user) {
+    return user.name + ' ' + user.lastname;
+  } else return 'Nieznajomy';
+}
+
+const element = <h1> Wiataj {showUser(User)}</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
